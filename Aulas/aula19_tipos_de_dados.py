d66c0c6e52be85d53a407926683281dd7b815717
Sergio Nibon <snibonjr@gmail.com>
2025-03-04 11:13:34 -0300

    atualização

--- Aulas/aula19_tipos_de_dados.py
+++ Aulas/aula19_tipos_de_dados.py
@@ -1,1 +1,19 @@
-teste
+# Strings -> São textos (caracteres)
+
+
+# Asps simples
+print('Sergio Nibon')
+print('Quando insiro aspas duplas com a simples, as duplas são evidenciadas -> ','Sergio "Nibon"', "\n")
+
+
+# Asps Duplas
+print("Sergio Nibon")
+print('Quando insiro aspas simples com as duplas, as simples são evidenciadas -> ',"Sergio 'Nibon'", "\n")
+
+# Escape
+print("Sergio \"Nibon\"", "\n")
+
+# r
+print(r"Sergio \"Nibon\"")
+
+
