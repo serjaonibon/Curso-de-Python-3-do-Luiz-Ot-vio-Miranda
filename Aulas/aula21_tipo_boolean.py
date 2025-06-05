f037ebbc88c7abd733d435a737bbba64bb633036
Sergio Nibon <snibonjr@gmail.com>
2025-06-05 03:56:10 -0300

    Atualização em 05-06-2025

--- Aulas/aula21_tipo_boolean.py
+++ Aulas/aula21_tipo_boolean.py
@@ -1,1 +1,5 @@
 # sim(true); não(false)
+
+print(10==10)
+print(10==9)
+print(type(10==10))
---
+++ Aulas/aula22_coersao_de_tipos.py
@@ -0,0 +1,23 @@
+# Coversão de tipos, coersão
+# type conversion, typecasting, coercion
+# É o ato de converter um tipo em outro
+# Tipos imutávei e primitivos:
+# str, int, bool
+
+print(1 + 2)
+print("a" + "b")
+print("1" + "2")
+print(int("1"), type(int("1"))) # Coersão
+print(int("1") + 2) # Nesse caso, fizemos a coersão e realizamos a soma
+
+
+print("##################################################################")
+# conversão de tipos, coerção
+# type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro
+#  tipos imutáveis e primitivos:
+# str, int, float, bool
+print(int('1'), type(int('1')))
+print(type(float('1') + 1))
+print(bool(' '))
+print(str(11) + 'b')
