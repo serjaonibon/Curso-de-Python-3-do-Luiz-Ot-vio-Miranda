43c87909dd65513103fa4e969c5a8d3a0e4fe0ba
Sergio Nibon <snibonjr@gmail.com>
2025-06-05 17:59:54 -0300

    05-06-2025

---
+++ Aulas/aula27_concatenação_(+)_e_repetição_(x)_com_operadores.py
@@ -0,0 +1,10 @@
+
+
+
+concatenacao = 'Luiz' + ' ' + 'Otávio' + str(10)
+print(concatenacao)
+informacao = "aqui o (*) sinal de mutiplicação repetirá a letra A 10 vezes"
+a_dez_vezes = 'A' * 10 + ' ' + informacao
+tres_vezes_luiz = 3 * 'Luiz'
+print(a_dez_vezes)
+print(tres_vezes_luiz)
---
+++ Aulas/aula29_exercício_de_programação_calculo_do_IMC_(Índice de Massa Corpórea)_+_Ellipsis.py
@@ -0,0 +1,12 @@
+
+#Calcular o índice de massa corporal
+nome = 'Sergio Nibon'
+altura = 1.86
+peso = 125
+IMC = int(peso / (altura * altura))
+
+print('-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+# print(nome + ', ' + 'seu peso é ' + peso + 'kg' + ' e seu IMC é' + IMC)
+print(nome + ', ' + 'seu peso é', peso,'kg' + ' e sua altuta é', altura)
+print('Desta forma, seu IMC corresponde a:', IMC)
+print('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-')
